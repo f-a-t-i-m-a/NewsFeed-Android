07d66b95f1f8b265902c372067a45e2d4b7ec1ba
Appson <fatemeh.farzian@gmail.com>
2016-11-07 00:21:50 +0330

    Successful build after adding Retrofit.

--- app/src/main/java/ir/appson/sportfeed/Feeds.java
+++ app/src/main/java/ir/appson/sportfeed/Feeds.java
@@ -1,18 +1,17 @@
 package ir.appson.sportfeed;
 
-import org.simpleframework.xml.Element;
-import org.simpleframework.xml.ElementList;
-import org.simpleframework.xml.Root;
 
 import java.util.List;
+
+import retrofit2.Call;
+
+import static ir.appson.sportfeed.proxy.RestClient.RequestMethod.GET;
 
 /**
  * Created by fatemeh on 11/6/2016.
  */
 
-@Root(name = "Feeds")
 public class Feeds {
-    @ElementList(inline = true)
-    List<FeedSummary> foodList;
+   List<FeedSummary> list;
 }
 
