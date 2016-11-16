079730d65dd187e4e952e6ff9b86366e22bf45d3
fatemeh <fatemeh.farzian@gmail.com>
2016-11-17 00:48:11 +0330

    All Asyncs removed and replaced with Retrofit :) except for images and thumbnails.

---
+++ app/src/main/java/ir/appson/sportfeed/views/channelnewspage/ChannelNEWSAPIService1.java
@@ -0,0 +1,16 @@
+package ir.appson.sportfeed.views.channelnewspage;
+
+import ir.appson.sportfeed.dto.ChannelNEWSRoot;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by fatemeh on 11/6/2016.
+ */
+public interface ChannelNEWSAPIService1 {
+
+    @GET("/api/mobile/feed/1/news/list/all")
+    Call<ChannelNEWSRoot> feeds();
+}
+
+
